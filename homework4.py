99a1a528242e308bad834d9f122cf2d5ca13b742
AlexTotoro <alexander.keepsolid@gmail.com>
2022-09-20 15:01:32 +0300

    test added

--- homework4.py
+++ homework4.py
@@ -19,6 +19,7 @@ from my_calc import *
 4.5. Создайте файл my_calc.py и пропишите в нем минимум 4 функции, выполняющие базовые арифметические вычисления. 
      Примените эти функции в качестве методов в другом файле. '''
 
+
 def square(square_side: int) -> tuple:
     return square_side * 4, square_side ** 2, square_side * sqrt(2)
 
@@ -36,13 +37,16 @@ def print_args(**kwargs):
 
 
 print_args(name='John', last_name='Smith', age=35, position='web developer')
+print('-' * 10)
+print_args(title='Достучаться до небес', director='Thomas Jahn', year=1997, budget=2200000, main_actor='Til Schweiger',
+           slogan='Быстрый автомобиль, миллион марок в багажнике, и всего одна неделя жить')
 
 print('-' * 10 + ' Task 4.3 ' + '-' * 10)
 my_list = [20, -3, 15, 2, -1, -21]
 print(f'Initial list: {my_list}')
 my_list_pos = list(filter(lambda x: x > 0, my_list))
 print(f'Positive values: {my_list_pos}')
-my_list_mul = reduce(lambda x, y: x*y, my_list)
+my_list_mul = reduce(lambda x, y: x * y, my_list)
 print(f'Multiplication result: {my_list_mul}')
 
 print('-' * 10 + ' Task 4.4 ' + '-' * 10)
@@ -52,4 +56,3 @@ print(mul_(2, 17))
 print(div_(128, 2))
 print(pow_(2, 10))
 print(div_(12, 0))
-
